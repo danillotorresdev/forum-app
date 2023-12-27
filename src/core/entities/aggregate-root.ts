b97534da527806881e8a184f31679aec2418e087
danillotorresdev <danilloept@gmail.com>
2023-12-27 17:16:16 -0300

    remove unsafed files

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,7 +1,7 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { DomainEvent } from '../events/domain-event'
 import { Entity } from './entity'
-
+// This is the base class for all Aggregate Roots in the application.
 export abstract class AggregateRoot<Props> extends Entity<Props> {
   private _domainEvents: DomainEvent[] = []
 
